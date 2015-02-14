228ef4ac6ca4df19f945b52ad2b4e5b373b771ff
tawago <orihaka.tawago@gmail.com>
2015-02-14 02:13:17 -0500

    add initial files

---
+++ front-page.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="/wp-content/themes/robobu/modules/jquery-fullPage.js"></script>
+		<script type="text/javascript" src=""></script>
+		<script type="text/javascript" src=""></script>
+		
+		<style>
+			body{
+				margin:0;
+				padding:0;
+			}
+			#sec1{
+				width: 100%;
+				height: 800px;
+				background: #fffeef;
+			}
+			#sec2{
+				width: 100%;
+				height: 1800px;
+			}
+		</style>
+	</head>
+	<body>
+		<section id="sec1">
+		</section>
+		<section id="sec2">
+		</section>
+		<section id="3">
+		</section>
+		<section>
+		</section>
+		<section>
+		</section>
+	</body>
+</html>

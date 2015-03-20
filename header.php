95587c676aee60907fa30b753d6d0ba7e5ecd01e
tawago <orihaka.tawago@gmail.com>
2015-03-20 19:19:48 +0900

    url修正

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" id="dashicons-css" href="http://ohpant.net/wp-includes/css/dashicons.min.css" type="text/css" media="all">
+	<link rel="stylesheet" id="dashicons-css" href="../wp-includes/css/dashicons.min.css" type="text/css" media="all">
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->

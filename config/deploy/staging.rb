a3a19ebbe2c53d36643cd24cc53178a113611bbb
tawago <orihaka.tawago@gmail.com>
2015-03-25 19:28:34 +0900

    fixing branch name

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 server '128.199.71.225', user: 'deploy', role: %w{web}
-set :branch, ENV["BRANCH_NAME"] || "staging"
+set :branch, fetch(ENV["BRANCH_NAME"], "staging")
 set :deploy_to, '/var/www/wp-content/themes/Robobu-staging'
 
 # role-based syntax

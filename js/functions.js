3831c22c6be47faf797fdd994010a021f838cc61
tawago <orihaka.tawago@gmail.com>
2015-04-16 17:57:25 +0900

    fix sidemenu

--- js/functions.js
+++ js/functions.js
@@ -87,7 +87,7 @@
 					$sidebar.attr( 'style', 'top: ' + topOffset + 'px;' );
 				} else if ( ! top && windowPos + adminbarOffset < $sidebar.offset().top ) {
 					top = true;
-					$sidebar.attr( 'style', 'position: fixed;' );
+					$sidebar.attr( 'style', 'position: fixed; left: 170%;' );
 				}
 			} else {
 				top = bottom = false;
@@ -96,7 +96,7 @@
 			}
 		} else if ( ! top ) {
 			top = true;
-			$sidebar.attr( 'style', 'position: fixed;' );
+			$sidebar.attr( 'style', 'position: fixed; left: 170%;' );
 		}
 
 		lastWindowPos = windowPos;

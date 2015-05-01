08c5db07a968a3a4430681331cc4908c8c3685f0
tawago <orihaka.tawago@gmail.com>
2015-05-01 17:25:13 +0900

    change font-size and edit thumbnail

--- functions.php
+++ functions.php
@@ -1,2 +1,23 @@
 <?php
 
+function twentyfifteen_post_thumbnail() {
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
+	if ( is_singular() ) :
+	?>
+
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail('medium'); ?>
+	</div><!-- .post-thumbnail -->
+
+	<?php else : ?>
+
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+		<?php
+			the_post_thumbnail( 'medium', array( 'alt' => get_the_title() ) );
+		?>
+	</a>
+
+	<?php endif; // End is_singular()
+}

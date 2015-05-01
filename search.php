67bb9102c8bc55f6771f299d0e147730fd6a576e
tawago <orihaka.tawago@gmail.com>
2015-05-01 16:54:48 +0900

    merge styles

--- search.php
+++ search.php
@@ -7,66 +7,7 @@
  * @since Twenty Fifteen 1.0
  */
 get_header(); ?>
-<style>
-@media screen and (min-width: 59.6875em){
-  body:before {
-    display: none;
-  }
-  .sidebar{
-    display: none;
-  }
-  .site-content{
-    width: 100%;
-    margin: 0 0 6%;
-    padding: 0 10%;
-  }
-  .page-header{
-    margin: 0; 
-    border: none;
-    padding: 0;
-    box-shadow: none;
-  }
-  .page-header .search-form:before{
-    position: absolute;
-    z-index: 20;
-    margin: 10px;
-    padding-right: 10px;
-    font: 400 40px/1 dashicons!important;
-    content: '\f179';
-    speak: none;
-    color: #707070;
-    color: rgba(51, 51, 51, 0.5);
-    border-right: 2px solid rgba(51, 51, 51, 0.5);
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  .page-header input[type="search"]{
-    height: 60px;
-    padding-left: 75px;
-  }
-  .page-header input::-webkit-input-placeholder{
-    font-size: 0.9em;
-    display: block;
-    height: 60px;
-  }
-  .hentry{
-    width: 30%;
-    float: left;
-    margin: 8.3333% 2.6em 0 0; 
-    padding-top: 6%;
-  }
-  .hentry:nth-child(4n){ margin: 0; margin-top: 8.3333%; /*検索結果の右列は右マージンなし*/ }
 
-  .hentry.has-post-thumbnail {
-    padding-top: 0;
-  }
-  .hentry .entry-title{
-    font-size: 1.2em;
-  }
-  .hentry .entry-summary{
-    font-size: 0.85em;
-  }
-</style>
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 

8842593463bc2fa55d8ac2f0924aefba32cfeb8c
tawago <orihaka.tawago@gmail.com>
2015-05-02 16:00:58 +0900

    thumbnail test

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function twentyfifteen_post_thumbnail() {
 
 	<?php else : ?>
 	
-	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id());?>)">
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id(), 'post-thumbnail');?>)">
 		<?php
 			the_post_thumbnail( 'small', array( 'alt' => get_the_title() ) );
 		?>

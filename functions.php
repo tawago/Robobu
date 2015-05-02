4b07e188365b26b5600649e715d4de6d2ccf4c72
tawago <orihaka.tawago@gmail.com>
2015-05-02 17:10:02 +0900

    thumbnail

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function twentyfifteen_post_thumbnail() {
 	
 	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" style="background: url(<?php $image= wp_get_attachment_image_src(get_post_thumbnail_id(), 'medium'); echo $image[0];?>)">
 		<?php
-			the_post_thumbnail( 'medium', array( 'alt' => get_the_title() ) );
+			//the_post_thumbnail( 'medium', array( 'alt' => get_the_title() ) );
 		?>
 	</a>
 

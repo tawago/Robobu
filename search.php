f9f9029627d168137d036b134816345a92339805
tawago <orihaka.tawago@gmail.com>
2015-05-02 17:38:34 +0900

    the first h2 or h3 in the content needs to be inline

--- search.php
+++ search.php
@@ -14,7 +14,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 			<header class="page-header">
 				<h1 style="display:none;"><?php printf( __( 'Search Results for: %s', 'twentyfifteen' ), get_search_query() ); ?></h1>
-				<form role="search" method="get" class="search-form" action="http://robobu.io/">
+				<form role="search" method="get" class="search-form" action="/">
 					<label>
 					
 						<input type="search" class="search-field" placeholder="<?php printf( __( 'Search Results for: %s', 'twentyfifteen' ), get_search_query() ); ?>" value="" name="s" title="検索:">

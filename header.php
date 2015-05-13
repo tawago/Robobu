314aff545b6e80883e75e33ab2481cf990f0e318
tawago <orihaka.tawago@gmail.com>
2015-05-13 21:25:44 +0900

    add GA

--- header.php
+++ header.php
@@ -13,6 +13,15 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+	<script>
+  	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+  	ga('create', 'UA-62908913-1', 'auto');
+  	ga('send', 'pageview');
+	</script>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link rel="stylesheet" id="dashicons-css" href="../wp-includes/css/dashicons.min.css" type="text/css" media="all">

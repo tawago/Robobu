3aafbdc4e8097e084f7fe01e7398ab27e7f927bc
tawago <orihaka.tawago@gmail.com>
2015-08-08 19:22:51 +0900

    test

--- header.php
+++ header.php
@@ -71,7 +71,7 @@
 		?></title>
 	<?php wp_head(); ?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script></script>
+	<script>$('article:nth-child(3n+1)').after('<div class="clearfix"></div>');</script>
 </head>
 
 <body <?php body_class(); ?>>

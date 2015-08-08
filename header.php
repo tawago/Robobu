50539b229790ca757e50969f8e621af88d646a25
tawago <orihaka.tawago@gmail.com>
2015-08-08 19:16:02 +0900

    test

--- header.php
+++ header.php
@@ -70,6 +70,8 @@
 		endif;
 		?></title>
 	<?php wp_head(); ?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script></script>
 </head>
 
 <body <?php body_class(); ?>>

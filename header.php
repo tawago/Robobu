8dddd2f16678b261dfe6aa8247fb454957d95e47
tawago <orihaka.tawago@gmail.com>
2015-08-20 19:03:19 +0900

    fix the sentence

--- header.php
+++ header.php
@@ -34,12 +34,10 @@
 					$sep = (empty($keywords)) ? '' : ', ';
 					$keywords .= $sep . $tag->name;
 				endforeach;
-	?>
-	<meta name="keywords" content="<?php echo $keywords; ?>" />
-	<?php
 			endif;
 		endif;
 	?>
+	<meta name="keywords" content="<?php echo $keywords; ?>" />
 	<script>
   	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
